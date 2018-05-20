b32d171b498b91c285fc9792dd60ff2177e869cd
RickBr0wn <ricky.brown.00@gmail.com>
2018-05-21 00:09:45 +0100

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+// Libraries
+import 'react-mdl/extra/material.css'
+import 'react-mdl/extra/material.js'
+
+// Compnents
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>My Site</h1>
-      </div>
-    );
+      <div style={{height: '300px', position: 'relative'}}>
+        <Layout fixedHeader>
+            <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content />
+        </Layout>
+    </div>
+    )
   }
 }
 
-export default App;
+export default App
