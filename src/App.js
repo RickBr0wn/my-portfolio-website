225f243f08d36ca83d77f85230e18e56fe0837d5
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-21 19:58:05 +0100

    Re-structured file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
+// Components
+import ButtonAppBar from './Components/ButtonAppBar'
+import LandingPage from './Pages/LandingPage'
+
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <ButtonAppBar />
+				<LandingPage />
     	</div>
     )
   }
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,10 +1,13 @@
 import React, { Component }from 'react'
 
+// Components
+
+
 class LandingPage extends Component{
 	render(){
 		return(
 			<div>
-				<h1> This is Landing Page</h1>
+				<h1>This is Landing Page</h1>
 			</div>
 		)
 	}
