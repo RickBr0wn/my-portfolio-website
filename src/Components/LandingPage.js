acbc72404c888f0e24ffdd1885bb3ae6a3682e3e
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-21 19:48:03 +0100

    Cleared exsisting code

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,40 +1,10 @@
 import React, { Component }from 'react'
 
-//Components
-import { Grid, Cell } from 'react-mdl'
-
 class LandingPage extends Component{
 	render(){
 		return(
-			<div style={{ width: "100%", margin: "auto" }}>
-				<Grid className="landing-grid">
-					<Cell col={12}>
-					 <img 
-						 src={require('../Avatar.png')}
-						 alt="avatar"
-						 className="avatar-image"
-					 />
-						<div className="banner-text">
-							<h1>Full Stack Web Developer</h1>
-							<hr />
-							<p>HTML | CSS | Bootstrap | JavaScript | ReactJS | NodeJS | Express | MongoDB | Johnny-Five | p5.js</p>
-							<div className="social-links">
-								<a href="https://twitter.com/riQeh_" rel="noopener noreferrer" target="_blank">
-									<i className="fab fa-twitter-square" aria-hidden="true" />
-								</a>
-								<a href="https://github.com/RickBr0wn" rel="noopener noreferrer" target="_blank">
-									<i className="fab fa-github-square" aria-hidden="true" />
-								</a>
-								<a href="https://www.freecodecamp.org/rickbr0wn" rel="noopener noreferrer" target="_blank">
-									<i className="fab fa-free-code-camp" aria-hidden="true" />
-								</a>
-								<a href="https://www.freecodecamp.org/rickbr0wn" rel="noopener noreferrer" target="_blank">
-									<i className="fas fa-at" aria-hidden="true" />
-								</a>
-							</div>
-					 	</div>
-					</Cell>
-				</Grid>
+			<div>
+				<h1> This is Landing Page</h1>
 			</div>
 		)
 	}
