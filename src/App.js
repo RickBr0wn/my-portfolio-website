9f93307043897ebe4586934e5c55b644046b2427
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-12 23:02:06 +0000

    failed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 
 // Components
-import ButtonAppBar from './Components/ButtonAppBar'
+import Main from ''
 import LandingPage from './Pages/LandingPage'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ButtonAppBar />
-				<LandingPage />
+				<Main />
     	</div>
     )
   }
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -4,7 +4,7 @@ class Projects extends Component{
 	render(){
 		return(
 			<div>
-				<h1> This is Projects Page</h1>
+				<h1>This is Projects Page</h1>
 			</div>
 		)
 	}
