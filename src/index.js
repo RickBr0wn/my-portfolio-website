f2acaa73adddef13b19e03c66186846a041a63b6
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-13 15:49:38 +0000

    Cleaned master branch to a 'cleaned' create-react-app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './Components/App'
+import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
 	<BrowserRouter>
 		<App />
-	</BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+	</BrowserRouter>, document.getElementById('root'))
+registerServiceWorker()
