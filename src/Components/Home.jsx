ac5994bfd96188424b380910560bf118a8ab2abd
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-13 17:18:15 +0000

    Hero image working with particles.js

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Particles from 'react-particles-js'
+import { params } from './HomePageComponents/ParticlesParams'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
-
+const Home = () => 
+      <div className="hero-image">
+        <Particles params={params} />
+        <div className="hero-text">
+          <h1>Rick Brown</h1>
+          <h3>Web Developer</h3>
+           
+        </div>
+      </div>
+  
 export default Home
---
+++ src/Components/HomePageComponents/ParticlesParams.js
@@ -0,0 +1,14 @@
+export const params = {
+    particles: {
+    number: {
+        value: 150
+    },
+    line_linked: {
+        shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 3
+        }
+    }
+    }
+}
