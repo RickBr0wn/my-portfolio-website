b6b97f8c2bf22a578787461ce6b6cc4bc5e79d9a
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-13 17:30:00 +0000

    Added hero text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,10 @@ const Home = () =>
         <div className="hero-text">
           <h1>Rick Brown</h1>
           <h3>Web Developer</h3>
-           
+          <div className="hero-social-icons">
+            <i class="fab fa-github"></i>
+            <i class="fab fa-twitter"></i>
+          </div>
         </div>
       </div>
   
